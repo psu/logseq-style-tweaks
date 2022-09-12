769497a380e97df52a4c1d5358bbe50feb0320a3
Pontus Sundén <pontus@psu.se>
2022-09-12 10:09:59 +0200

    adjust opacity for meta headings

--- tweaks/harmonise-meta-headings.js
+++ tweaks/harmonise-meta-headings.js
@@ -3,7 +3,7 @@ const style = `
 .page-hierarchy h2.font-bold.opacity-30,
 .custom-query-title .title-text {
   font-weight: 600;
-  opacity: 0.65;
+  opacity: 0.75;
   color: var(--ls-primary-text-color);
 }
 `
