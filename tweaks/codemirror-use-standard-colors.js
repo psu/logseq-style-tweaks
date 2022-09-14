9cede56b844b7e36155082930f82967ea848ebd3
Pontus Sundén <pontus@psu.se>
2022-09-15 00:05:50 +0200

    fix codemirror tweak

--- tweaks/codemirror-use-standard-colors.js
+++ tweaks/codemirror-use-standard-colors.js
@@ -1,12 +1,24 @@
 const style = `
+.cm-s-solarized.cm-s-light {
+  background-color: var(--ls-quaternary-background-color);
+  text-shadow: none;
+  color: var(--ls-secondary-text-color);
+}
 .cm-s-solarized.cm-s-dark {
   background-color: var(--ls-tertiary-background-color);
   text-shadow: none;
-  color: var(--ls-secondary-text-color)
+  color: var(--ls-secondary-text-color);
+}
+.extensions__code-lang,
+.cm-s-solarized.cm-s-light .CodeMirror-gutters {
+  background-color: var(--ls-tertiary-background-color);
+  text-shadow: none;
+  color: var(--ls-primary-text-color);
 }
 .cm-s-solarized.cm-s-dark .CodeMirror-gutters {
   background-color: var(--ls-quaternary-background-color);
-  color: var(--ls-secondary-text-color)
+  text-shadow: none;
+  color: var(--ls-secondary-text-color);
 }
 `
 export default style
