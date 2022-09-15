fdb8cbc9103fb9a2d4b5f927040a1b5d57d2a18d
Pontus Sundén <pontus@psu.se>
2022-09-15 17:58:47 +0200

    fix reset tippy popper tweak

--- tweaks/reset-tippy-popper.js
+++ tweaks/reset-tippy-popper.js
@@ -1,16 +1,16 @@
 const style = `
 .tippy-wrapper {
-	background-color: var(--ls-secondary-background-color);
+	background-color: var(--ls-tertiary-background-color);
 	color: var(--ls-primary-text-color);
 	font-weight: 400!important;
   font-size: 0.9rem;
   border-radius: 6px;
 }
 .tippy-tooltip {
-	border-top: 1px solid var(--ls-secondary-border-color);
-	border-right: 1px solid var(--ls-secondary-border-color);
-	border-bottom: 1px solid var(--ls-secondary-border-color);
-	border-left: 1px solid var(--ls-secondary-border-color);
+	border-top: 1px solid var(--ls-tertiary-border-color);
+	border-right: 1px solid var(--ls-tertiary-border-color);
+	border-bottom: 1px solid var(--ls-tertiary-border-color);
+	border-left: 1px solid var(--ls-tertiary-border-color);
 	box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),0 4px 6px -2px rgba(0, 0, 0, 0.05);
 	--webkit-box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),0 4px 6px -2px rgba(0, 0, 0, 0.05);
 }
