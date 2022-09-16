235de8a6cd4d36a5caa6c224e40a278875c46fbc
Pontus Sundén <pontus@psu.se>
2022-09-16 12:10:41 +0200

    add light/dark theme separation to meta headings

--- tweaks/harmonise-meta-headings.js
+++ tweaks/harmonise-meta-headings.js
@@ -1,9 +1,16 @@
 const style = `
-.references h2.font-bold.opacity-80,
-.page-hierarchy h2.font-bold.opacity-30,
-.custom-query-title .title-text {
+.dark-theme .references h2.font-bold.opacity-80,
+.dark-theme .page-hierarchy h2.font-bold.opacity-30,
+.dark-theme .custom-query-title .title-text {
   font-weight: 600;
   opacity: 0.7;
+  color: var(--ls-primary-text-color);
+}
+.light-theme .references h2.font-bold.opacity-80,
+.light-theme .page-hierarchy h2.font-bold.opacity-30,
+.light-theme .custom-query-title .title-text {
+  font-weight: 600;
+  opacity: 0.85;
   color: var(--ls-primary-text-color);
 }
 `
